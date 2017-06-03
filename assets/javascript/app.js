4d9d2d29f199a5f46bf35a515c6bfa3459fb312f
Amy Volkert <Amy Volkert>
2017-06-03 05:48:42 -0600

    added variables and initial html

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+//Trivia Game
+$(document).ready(function() {
+	 
+	//Add question variables
+	var stem;
+	var answer;
+	var threeDistractors= [],
+	var distractor;
+	//Add outcome variables
+	var numberRight=0;
+	var numberWrong=0;
+	var notAnswered=0;
+	//Add timer variables
+	var clockRunning = false; //Needed?
+	
+
+//document.ready function ends
+});
